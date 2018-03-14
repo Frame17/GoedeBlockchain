a20d1b6863957118f4543d427add18b59911cf34
Vladyslav Bozhko <drakosvlad@gmail.com>
2018-03-14 13:29:42 +0200

    Trial and error

--- goede-electron-client/js/chain.js
+++ goede-electron-client/js/chain.js
@@ -1,19 +1,38 @@
-const openchain = require("openchain");
-const bitcore = require("bitcore-lib");
+var openchain = require("openchain");
+var bitcore = require("bitcore-lib");
+var Mnemonic = require("bitcore-mnemonic");
 
-var client = new openchain.ApiClient("https://openchain.bozhko.net/");
+var chain = {
+    client: new openchain.ApiClient("https://openchain.bozhko.net/"),
+    state: 0,
+    privateKey: "",
+    address: "",
+    signer: "",
+    dataPath: "",
+    derivedKey: "",
+    loadKeyFromSeed: function (seed) {
+        // Load a private key from a seed
+        this.privateKey = new bitcore.HDPrivateKey.fromSeed(seed, "openchain");
+        this.loadKeyData();
+    },
+    loadKeyFromMnemonic: function (mnemonic) {
+        if(Mnemonic.isValid(mnemonic)) {
+            var code = new Mnemonic(mnemonic);
+            this.privateKey = new bitcore.HDPrivateKey(code.toHDPrivateKey(null, "livenet").xprivkey.data);
+            this.privateKey.network = bitcore.Networks.get("openchain");
+            this.derivedKey = this.privateKey.derive(44, true).derive(64, true).derive(0, true).derive(0).derive(0);
+            this.loadKeyData();
+            return true;
+        }
+        return false;
+    },
+    loadKeyData: function() {
+        this.address = this.derivedKey.privateKey.toAddress();
+        // Calculate the accounts corresponding to the private key
+        this.dataPath = "/asset/p2pkh/" + this.address + "/metadata/";
 
-var privateKey;
-var address;
-var signer;
-var dataPath;
-
-exports.loadKeyFromSeed = function (seed) {
-    // Load a private key from a seed
-    privateKey = new bitcore.HDPrivateKey.fromSeed(seed, "openchain");
-    address = privateKey.publicKey.toAddress();
-    // Calculate the accounts corresponding to the private key
-    dataPath = "/asset/p2pkh/" + address + "/metadata/";
-
-    signer = new openchain.MutationSigner(privateKey);
+        this.signer = new openchain.MutationSigner(this.privateKey);
+    }
 }
+
+module.exports = chain
--- goede-electron-client/renderer.js
+++ goede-electron-client/renderer.js
@@ -4,14 +4,38 @@ const chain = require('./js/chain.js');
 const m = require('mithril')
 
 //Views
-const WelcomeComponent = {
+var LoginComponent = {
+    username: "",
+    setUsername: function(name) {
+        this.username = name
+    },
+    login: function() {
+        authorize(this.username)
+    },
     view: function() {
         return m("div", [
             m("h2", {class: "title"}, "Goede Client"),
-            m("input", {type: "text", name: "seed"}),
-            m("button", {onclick: function() {getKeyFromSeed(document.getElementsByName('seed')[0].value)}}, "Load data"),
+            m("input", {type: "text", name: "seed", oninput: m.withAttr("value", this.setUsername.bind(this)), value: this.username}),
+            m("button", {onclick: this.login.bind(this)}, "Load data"),
         ])
     }
 }
 
-m.mount(root, WelcomeComponent)
+var WalletComponent = {
+    view: function() {
+        return m("div", [
+            m("h2", chain.address.toString())
+        ])
+    }
+}
+
+function authorize(username) {
+    if(chain.loadKeyFromMnemonic(username)) {
+        m.route.set("/wallet")
+    }
+}
+
+m.route(root, "/login", {
+    "/login": LoginComponent,
+    "/wallet": WalletComponent
+})

109e3a656a682c9123469738c5b88a8c2d8eee66
Frame_17 <fedusov.serhiy@gmail.com>
2018-04-24 15:33:57 +0300

    Application UI

--- goede-electron-client/js/renderer.js
+++ goede-electron-client/js/renderer.js
@@ -13,19 +13,27 @@ var peer = undefined
 //Views
 var LoginComponent = {
     password: "",
-    setPassword: function(pwd) { this.password = pwd },
-    login: function() {
-        if(filesystem.unlock(this.password) != "Error") {
+    setPassword: function (pwd) {
+        this.password = pwd
+    },
+    login: function () {
+        if (filesystem.unlock(this.password) != "Error") {
             initPeer(filesystem.data.address)
             authorize(filesystem.mnemonic)
         } else {
             alert("Incorrect password")
         }
     },
-    view: function() {
+    view: function () {
         return m("div", [
             m("h2", {class: "title"}, "Goede Client Login"),
-            m("input", {type: "password", name: "pwd", placeholder: "Master password", oninput: m.withAttr("value", this.setPassword.bind(this)), value: this.password}),
+            m("input", {
+                type: "password",
+                name: "pwd",
+                placeholder: "Master password",
+                oninput: m.withAttr("value", this.setPassword.bind(this)),
+                value: this.password
+            }),
             m("button", {class: "button-primary", onclick: this.login.bind(this)}, "Login"),
             m("div", {class: "row"}, [
                 m("a", {href: "#!/register"}, "Register")
@@ -38,10 +46,16 @@ var RegisterComponent = {
     email: "",
     username: "",
     password: "",
-    setEmail: function(email) { this.email = email },
-    setUsername: function(name) { this.username = name },
-    setPassword: function(pwd) { this.password = pwd },
-    login: function() {
+    setEmail: function (email) {
+        this.email = email
+    },
+    setUsername: function (name) {
+        this.username = name
+    },
+    setPassword: function (pwd) {
+        this.password = pwd
+    },
+    login: function () {
         filesystem.writeMnemonicToFile(this.username, this.password)
         filesystem.setData({
             name: "",
@@ -51,16 +65,37 @@ var RegisterComponent = {
         initPeer(this.email)
         authorize(this.username)
     },
-    generatePassphrase: function() {
+    generatePassphrase: function () {
         this.username = new Mnemonic().toString()
     },
-    view: function() {
+    view: function () {
         return m("div", [
             m("h2", {class: "title"}, "Goede Client Registration"),
-            m("input", {type: "email", name: "email", placeholder: "Email", oninput: m.withAttr("value", this.setEmail.bind(this)), value: this.email}),
-            m("input", {type: "text", name: "seed", placeholder: "Passphrase", oninput: m.withAttr("value", this.setUsername.bind(this)), value: this.username}),
-            m("input", {type: "password", name: "pwd", placeholder: "New master password", oninput: m.withAttr("value", this.setPassword.bind(this)), value: this.password}),
-            m("button", {class: "button-primary", onclick: this.generatePassphrase.bind(this)}, "Generate new passphrase"),
+            m("input", {
+                type: "email",
+                name: "email",
+                placeholder: "Email",
+                oninput: m.withAttr("value", this.setEmail.bind(this)),
+                value: this.email
+            }),
+            m("input", {
+                type: "text",
+                name: "seed",
+                placeholder: "Passphrase",
+                oninput: m.withAttr("value", this.setUsername.bind(this)),
+                value: this.username
+            }),
+            m("input", {
+                type: "password",
+                name: "pwd",
+                placeholder: "New master password",
+                oninput: m.withAttr("value", this.setPassword.bind(this)),
+                value: this.password
+            }),
+            m("button", {
+                class: "button-primary",
+                onclick: this.generatePassphrase.bind(this)
+            }, "Generate new passphrase"),
             m("button", {class: "button-primary", onclick: this.login.bind(this)}, "Save credentials")
         ])
     }
@@ -69,22 +104,50 @@ var RegisterComponent = {
 var WalletComponent = {
     key: "",
     value: "",
-    setKey: function(val) { this.key = val },
-    setValue: function(val) { this.value = val},
-    storeData: function() { storeData(this.key, this.value) },
-    retrieveData: function() { this.value = retrieveData(this.key) },
-    view: function() {
+    setKey: function (val) {
+        this.key = val
+    },
+    setValue: function (val) {
+        this.value = val
+    },
+    storeData: function () {
+        storeData(this.key, this.value)
+    },
+    retrieveData: function () {
+        this.value = retrieveData(this.key)
+    },
+    logOut : function () {
+        m.route.set("/login")
+    },
+    view: function () {
         return m("div", [
             m("h3", chain.address.toString()),
             m("div", {class: "row"}, [
-                m("input", {class: "six columns", type: "text", placeholder: "Key", name: "key", oninput: m.withAttr("value", this.setKey.bind(this)), value: this.key})
+                m("input", {
+                    class: "six columns",
+                    type: "text",
+                    placeholder: "Key",
+                    name: "key",
+                    oninput: m.withAttr("value", this.setKey.bind(this)),
+                    value: this.key
+                })
             ]),
             m("div", {class: "row"}, [
-                m("input", {class: "six columns", type: "text", placeholder: "Value", name: "value", oninput: m.withAttr("value", this.setValue.bind(this)), value: this.value})
+                m("input", {
+                    class: "six columns",
+                    type: "text",
+                    placeholder: "Value",
+                    name: "value",
+                    oninput: m.withAttr("value", this.setValue.bind(this)),
+                    value: this.value
+                })
             ]),
             m("div", {class: "row"}, [
                 m("button", {class: "three columns", onclick: this.storeData.bind(this)}, "Store"),
                 m("button", {class: "three columns", onclick: this.retrieveData.bind(this)}, "Retrieve")
+            ]),
+            m("div", {class: "row"}, [
+                m("button", {class: "six columns", onclick: this.logOut.bind(this)}, "Log out"),
             ]),
             m("div", {class: "row"}, [
                 m("a", {href: "#!/data"}, "Your data")
@@ -97,16 +160,34 @@ var WalletComponent = {
 }
 
 var DataComponent = {
-    setName: function(val) { filesystem.data.name = val },
-    setAddress: function(val) { filesystem.data.address = val },
-    view: function() {
+    setName: function (val) {
+        filesystem.data.name = val
+    },
+    setAddress: function (val) {
+        filesystem.data.address = val
+    },
+    view: function () {
         return m("div", [
             m("h3", "Your data"),
             m("div", {class: "row"}, [
-                m("input", {class: "six columns", type: "text", placeholder: "Name", name: "name", oninput: m.withAttr("value", this.setName.bind(this)), value: filesystem.data.name})
+                m("input", {
+                    class: "six columns",
+                    type: "text",
+                    placeholder: "Name",
+                    name: "name",
+                    oninput: m.withAttr("value", this.setName.bind(this)),
+                    value: filesystem.data.name
+                })
             ]),
             m("div", {class: "row"}, [
-                m("input", {class: "six columns", type: "email", placeholder: "Name", name: "address", oninput: m.withAttr("value", this.setAddress.bind(this)), value: filesystem.data.address})
+                m("input", {
+                    class: "six columns",
+                    type: "email",
+                    placeholder: "Name",
+                    name: "address",
+                    oninput: m.withAttr("value", this.setAddress.bind(this)),
+                    value: filesystem.data.address
+                })
             ]),
             m("div", {class: "row"}, [
                 m("button", {class: "three columns", onclick: writeUserData}, "Save")
@@ -121,19 +202,37 @@ var DataComponent = {
 var PeerComponent = {
     data: "",
     receiverAddress: "",
-    setData: function(val) { this.data = val },
-    setReceiverAddress: function(val) { this.receiverAddress = val },
-    sendData: function() {
+    setData: function (val) {
+        this.data = val
+    },
+    setReceiverAddress: function (val) {
+        this.receiverAddress = val
+    },
+    sendData: function () {
         p2p.sendData(peer, this.receiverAddress, this.data);
     },
-    view: function() {
+    view: function () {
         return m("div", [
             m("h3", "Send data"),
             m("div", {class: "row"}, [
-                m("input", {class: "six columns", type: "text", placeholder: "Data", name: "data", oninput: m.withAttr("value", this.setData.bind(this)), value: this.data})
+                m("input", {
+                    class: "six columns",
+                    type: "text",
+                    placeholder: "Data",
+                    name: "data",
+                    oninput: m.withAttr("value", this.setData.bind(this)),
+                    value: this.data
+                })
             ]),
             m("div", {class: "row"}, [
-                m("input", {class: "six columns", type: "email", placeholder: "Send to", name: "receiverAddress", oninput: m.withAttr("value", this.setReceiverAddress.bind(this)), value: this.receiverAddress})
+                m("input", {
+                    class: "six columns",
+                    type: "email",
+                    placeholder: "Send to",
+                    name: "receiverAddress",
+                    oninput: m.withAttr("value", this.setReceiverAddress.bind(this)),
+                    value: this.receiverAddress
+                })
             ]),
             m("div", {class: "row"}, [
                 m("button", {class: "three columns", onclick: this.sendData.bind(this)}, "Send")
@@ -146,7 +245,7 @@ var PeerComponent = {
 }
 
 function authorize(username) {
-    if(chain.loadKeyFromMnemonic(username)) {
+    if (chain.loadKeyFromMnemonic(username)) {
         m.route.set("/wallet")
     }
 }
@@ -163,11 +262,13 @@ function writeUserData() {
     filesystem.writeData()
 }
 
+
+
 function initPeer(address) {
     peer = p2p.createPeer(address);
     //Await connections from other peers
     peer.on('connection', p2p.getData);
-    peer.on('error', function(err) {
+    peer.on('error', function (err) {
         console.log(err);
     })
 
@@ -181,10 +282,6 @@ m.route(root, "/register", {
     "/peer": PeerComponent
 })
 
-if(filesystem.mnemonicFileExists()) {
+if (filesystem.mnemonicFileExists()) {
     m.route.set("/login")
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1d0a8d2953b3f59aed8ffddf01d0ede868472086
